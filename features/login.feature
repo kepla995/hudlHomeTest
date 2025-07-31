e3fe9b8b00a576d5850557c93c1667e66fce35a7
kepla995 <jbantock95@gmail.com>
2025-07-31 12:14:06 +0100

    initial start 4 login with google test

--- features/login.feature
+++ features/login.feature
@@ -1,25 +1,10 @@
 Feature: Login User via various methods
 
-    Scenario: Non-registered User cannot login
-        Given I am on the login page
-        When I enter an email
-        And I enter a password
-        And I press the login button
-        Then I should not be able to login
-        And I should see incorrect username or password error message 
-    
-    Scenario: Email User can login
-        Given I am on the login page
-        When I enter a valid email
-        And I enter a valid password
-        And I press the login button
-        Then I should be logged in successfully
-        And I can logout successfully
 
      Scenario: Google User can login
         Given I am on the login page
         When I select the Google login option
-        And I enter my Google credentials
+        And I enter Google credentials
         And I press the login button
         Then I should be logged in successfully
         And I can logout successfully
@@ -39,5 +24,4 @@ Feature: Login User via various methods
         And I press the login button
         Then I should be logged in successfully
         And I can logout successfully
-
 
--- steps/google_steps.py
+++ steps/google_steps.py
@@ -1,10 +1,11 @@
-
+from seleniumbase import SB
 from behave import when
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
+GOOGLE_EMAIL_INPUT_LOCATOR = (By.ID, "identifierId")
 
 @when('I select the Google login option')
 def step_impl_select_google_login(context):
@@ -21,19 +22,42 @@ def step_impl_select_google_login(context):
         raise Exception(f"Could not find or click Google login button: {e}")
 
 
-@when('I enter my Google credentials')
+@when('I enter Google credentials')
 def step_impl_enter_google_credentials(context):
-    """
-    Finds the Google email and password input fields and enters placeholder credentials.
-    """
-    print("Entering Google credentials...")
+
     try:
         email_input = context.driver.find_element(*GOOGLE_EMAIL_INPUT_LOCATOR)
         email_input.send_keys("jbantock95@gmail.com")
         print("Email entered.")
-        time.sleep(120)
-        next_button = context.driver.find_element(By.CLASS_NAME, "VfPpkd-vQzf8d")
+        time.sleep(1)
+        next_button = context.driver.find_element(By.CLASS_NAME, "VfPpkd-*")
         next_button.click()
         print("next button clicked.")
     except Exception as e:
         raise Exception(f"Could not find or interact with email input using {GOOGLE_EMAIL_INPUT_LOCATOR}: {e}")
+
+#   Cypress.Commands.add("googleOneTapSignIn", () => {
+#   cy.log("Sign-In");
+#   cy.log(Cypress.env("GOOGLE_REGISTER_CLIENT_ID"));
+#   cy.log(Cypress.env("GOOGLE_REGISTER_CLIENT_SECRET"));
+#   cy.log(Cypress.env("GOOGLE_REGISTER_REFRESH_TOKEN"));
+#   cy.request({
+#     method: "POST",
+#     url: "https://www.googleapis.com/oauth2/v4/token",
+#     body: {
+#       grant_type: "refresh_token",
+#       client_id: Cypress.env("GOOGLE_REGISTER_CLIENT_ID"),
+#       client_secret: Cypress.env("GOOGLE_REGISTER_CLIENT_SECRET"),
+#       refresh_token: Cypress.env("GOOGLE_REGISTER_REFRESH_TOKEN"),
+#     },
+#   }).then(({ body }) => {
+#     const { id_token } = body;
+#     cy.wait(10000);
+#     cy.window().then((window) => {
+#       cy.log("Passing credential in to Google");
+#       window.handleCredentialResponse({
+#         credential: id_token,
+#       });
+#     });
+#   });
+# });
